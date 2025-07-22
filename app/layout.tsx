59e0f793c418dfc438746950d6b502707e5b892f
duckduck33 <dukekk33@gmail.com>
2025-07-22 17:50:32 +0900

    방문자통계대시보드2

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,15 +46,15 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
-        {/* Google Tag Manager Head 코드 */}
+        {/* Google Analytics GA4 코드 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C8SVY8E8RQ"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-PWX8GG8M');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-C8SVY8E8RQ');
             `
           }}
         />
@@ -62,13 +62,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* Google Tag Manager Body 코드 */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWX8GG8M"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-          }}
-        />
         {children}
       </body>
     </html>
